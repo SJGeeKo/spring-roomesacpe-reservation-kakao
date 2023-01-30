d752d3a37abf56cbfe22b45532c59079eedc2d69
SJGeeKo <sungjun.gee.ko@gmail.com>
2023-01-30 11:33:17 +0900

    feat: RoomEscapeApplication 클래스 내 ApplicationContext를 BeanContext 객체에 주입하는 로직 작성

--- src/main/java/nextstep/RoomEscapeApplication.java
+++ src/main/java/nextstep/RoomEscapeApplication.java
@@ -1,15 +1,31 @@
 package nextstep;
 
+import nextstep.config.BeanContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class RoomEscapeApplication {
 
+    private final ApplicationContext appContext;
+
+    public RoomEscapeApplication(ApplicationContext appContext) {
+        this.appContext = appContext;
+    }
+
     public static void main(String[] args) {
         // 스프링 애플리케이션 실행
         SpringApplication.run(RoomEscapeApplication.class, args);
+
         // 콘솔 애플리케이션 실행
         ConsoleEscapeApplication.run();
+    }
+
+    @PostConstruct
+    public void init() {
+        BeanContext.init(appContext);
     }
 }
